70c84bf9d19b41373a848734182f81cdfe933f59
bperalta14 <briannaperalta@pursuit.org>
2023-10-16 15:56:56 -0400

    error handling lab commit

--- index.js
+++ index.js
@@ -19,6 +19,9 @@ const exampleProducts = [
   - The `cart` array is empty.
 */
 function getCartTotal(cart) {
+  if (cart.length === 0) {
+    throw new Error("The cart is empty.");
+  }
   let result = 0;
   for (let product of cart) {
     result += product.priceInCents;
@@ -36,8 +39,17 @@ function getCartTotal(cart) {
   - Any of the products in the `products` array does not have a `priceInCents` key.
 */
 function filterProductsByPriceRange(products, min, max) {
+  if (products.length === 0) {
+    throw new Error("The products array is empty.");
+  }
+  if (typeof min !== 'number' || typeof max !== 'number' || min < 0 || max <= 0 || min > max) {
+    throw new Error("Invalid min and max values.");
+  }
   const result = [];
   for (let product of products) {
+    if (!product.priceInCents) {
+      throw new Error("Product does not have a priceInCents key.");
+    }
     if (product.priceInCents >= min && product.priceInCents <= max) {
       result.push(product);
     }
@@ -49,10 +61,14 @@ function filterProductsByPriceRange(products, min, max) {
   If any errors occur in this function, it should return `0`.
 */
 function getTotalOfAllProductsByPriceRange(products, min, max) {
-  const filteredProducts = filterProductsByPriceRange(products, min, max);
-  const total = getCartTotal(filteredProducts);
-
-  return total;
+  try {
+    const filteredProducts = filterProductsByPriceRange(products, min, max);
+    const total = getCartTotal(filteredProducts);
+    return total;
+  } catch (error) {
+    console.error(error.message);
+    return 0;
+  }
 }
 
 module.exports = {
